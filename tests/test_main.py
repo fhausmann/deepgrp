3376f916f6dd1ad184927c765d5d62acaa3541bc
Fabian Hausmann <fabian.hausmann@zmnh.uni-hamburg.de>
2021-06-05 22:14:26 +0200

    Add test template for __main__

---
+++ tests/test_main.py
@@ -0,0 +1,43 @@
+"""Test deepgrp.__main__."""
+
+import pytest
+
+
+class TestCommandLineParser:
+    @pytest.mark.xfail()
+    def test_init(self):
+        raise NotImplementedError()
+
+    @pytest.mark.xfail()
+    def test_predict(self):
+        raise NotImplementedError()
+
+    @pytest.mark.xfail()
+    def test_train(self):
+        raise NotImplementedError()
+
+    @pytest.mark.xfail()
+    def test_run(self):
+        raise NotImplementedError()
+
+    @pytest.mark.xfail()
+    def test_set_logging(self):
+        raise NotImplementedError()
+
+    @pytest.mark.xfail()
+    def test_setup_tensorflow(self):
+        raise NotImplementedError()
+
+    @pytest.mark.xfail()
+    def test_parse_args(self):
+        raise NotImplementedError()
+
+
+@pytest.mark.xfail()
+def test_predict():
+    raise NotImplementedError()
+
+
+@pytest.mark.xfail()
+def test__read_multi_fasta():
+    raise NotImplementedError()
