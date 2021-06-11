29458999bdfffb2dff0ab8e1ac256db08f33f2b0
Julian Klemm <julian.klemm1@gmail.com>
2021-06-11 18:12:00 +0200

    tests_main WIP

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,9 +12,33 @@ class TestCommandLineParser:
     def test_predict(self):
         raise NotImplementedError()
 
+    def get_toml_dummy():
+        # get dgmodel.Options that is initialised
+        # maybe not necessary, depending on how preprocessing and training
+        # are tested
+        raise NotImplementedError()
+
+    def rep_dg_train_training():
+        # load model instead?
+        return None
+
     @pytest.mark.xfail()
     def test_train(self):
-        raise NotImplementedError()
+        #Overwrite parameters
+        monkeypatch.setattr(dgmodel.Options, "from_toml", get_toml_dummy())
+        monkeypatch.setattr(parameter, "fromdict", lambda _func, **unused: None)
+
+        #logdir
+        monkeypatch.setattr(os, "mkdir", lambda _func, **unused: None)
+
+        # preprocessing
+
+
+        # training - skip training and load a model instead due to intense runtime
+        monkeypatch.setattr(dgtrain, "training", rep_dg_train_training)
+        monkeypatch.setattr(model, "save", lambda _func, **unused: None)
+
+
 
     @pytest.mark.xfail()
     def test_run(self):
